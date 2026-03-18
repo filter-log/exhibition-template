3bbbe5b44997caa07d9773ac3983dda151b1b4fa
Seobeen Jang <saudko-1@naver.com>
2026-03-18 21:25:21 +0900

    Update config.js

--- assets/js/config.js
+++ assets/js/config.js
@@ -1,9 +1,8 @@
 window.EXHIBITION_CONFIG = {
-  repoName: "exhibition-template",
-  siteUrl: "https://filter-log.github.io/exhibition-template",
-  workerApiUrl: "",
-  pagesCmsUrl: "https://app.pagescms.org/filter-log/exhibition-template/main",
+  repoName: "exhibition-4th",
+  siteUrl: "https://filter-log.github.io/exhibition-4th",
+  workerApiUrl: "https://exhibition-worker.filter-log.workers.dev/",
+  pagesCmsUrl: "https://app.pagescms.org/filter-log/exhibition-4th/main",
   maxArtworkDescriptionLength: 200,
   maxArtistDescriptionLength: 500
 };
-
